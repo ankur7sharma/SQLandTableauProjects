b9f8eab1f0ef09c0f61deb6a47d0053f056ff486
ankur7sharma <100619273+ankur7sharma@users.noreply.github.com>
2022-03-02 22:50:18 +0530

    Update 5. Enter data in T2 covid_vaccinations.sql

--- Project 1 - Covid Data/Part 1 - SQL (Data Exploration)/5. Enter data in T2 covid_vaccinations.sql
+++ Project 1 - Covid Data/Part 1 - SQL (Data Exploration)/5. Enter data in T2 covid_vaccinations.sql
@@ -1,4 +1,5 @@
 LOAD DATA LOCAL INFILE 'D:/Ankur/2020-Finale/Online Courses/Projects for Resume/Data Analyst Portfolio Project 1/T2- Covid Vaccinations.csv'
+-- In the LOAD DATA command, you have to write the directory of the place where you have saved your Covid Vaccinations csv file
 INTO TABLE covid_vaccinations
 FIELDS TERMINATED BY ','
 IGNORE 1 ROWS;
